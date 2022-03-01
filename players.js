3dcf767979e3d24b4be188825965c1b4bb9c31a0
Alex Waller <alex@artofalexwaller.com>
2022-03-01 02:58:49 -0500

    playerList

--- players.js
+++ players.js
@@ -1,2 +1,20 @@
 // players page
 console.log("Hello Oa");
+
+console.log("------------------------------");
+console.log("--------------------------------");
+
+const playersPageHeader = document.getElementById("players-page-header");
+
+const playersFormDiv = document.getElementById("players-form-div");
+
+const playerNameInput = document.getElementById("player-name-input");
+
+const playerHometownInput = document.getElementById("player-hometown-input");
+
+const playerBioInput = document.getElementById("player-bio-input");
+
+const playerSubmitBtn = document.getElementById("player-submit-btn");
+
+console.log("------------------------------");
+console.log("-------------------------------");
